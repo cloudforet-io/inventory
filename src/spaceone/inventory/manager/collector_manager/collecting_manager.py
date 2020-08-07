6cde826bdc13fc1730d2292b8c70048374a4ffd1
Jihyung Song <bluese@megazone.com>
2020-08-07 11:14:36 +0900

    Add Debuging message

--- src/spaceone/inventory/manager/collector_manager/collecting_manager.py
+++ src/spaceone/inventory/manager/collector_manager/collecting_manager.py
@@ -206,6 +206,11 @@ class CollectingManager(BaseManager):
                 # Update
                 _LOGGER.debug('[_process_single_result] Update resource.')
                 data.update(res_info[0])
+
+                _LOGGER.debug("------------------------------------ UPDATE ------------------------------")
+                _LOGGER.debug(data)
+                _LOGGER.debug("------------------------------------ UPDATE ------------------------------")
+
                 res_msg = svc.update(data)
                 job_mgr.increase_updated_count(params['job_id'], domain_id)
             elif total_count > 1:
