93c3b1d9b5ff7a3ea5c1a7ff5ac9779b2ad31232
Jongmin Kim <whdalsrnt@megazone.com>
2021-02-17 18:22:08 +0900

    Fix a data merge mode bug
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/inventory/manager/collection_data_manager.py
+++ src/spaceone/inventory/manager/collection_data_manager.py
@@ -133,9 +133,10 @@ class CollectionDataManager(BaseManager):
         pinned_keys = collection_info.get('pinned_keys', [])
         state = collection_info['state']
 
-        _LOGGER.debug('------------------')
-        _LOGGER.debug(f'Update Mode: {self.update_mode}')
-        _LOGGER.debug(f'[update_request_data: data.power_state] {self.merged_data.get("data", {}).get("power_state")}')
+        # _LOGGER.debug('------------------')
+        # _LOGGER.debug(f'Update Mode: {self.update_mode}')
+        # _LOGGER.debug(f'[update_request_data: data.power_state] {resource_data.get("data", {}).get("power_state")}')
+        # _LOGGER.debug(f'[update_request_data: data.compute] {resource_data.get("data", {}).get("compute")}')
 
         if self.updated_by != 'manual':
             if self.updated_by not in all_secrets:
@@ -180,7 +181,8 @@ class CollectionDataManager(BaseManager):
         garbage_collection[self.updated_by] = self.job_id
         self.merged_data['garbage_collection'] = garbage_collection
 
-        _LOGGER.debug(f'[merged_data: data.power_state] {self.merged_data.get("data", {}).get("power_state")}')
+        # _LOGGER.debug(f'[merged_data: data.power_state] {self.merged_data.get("data", {}).get("power_state")}')
+        # _LOGGER.debug(f'[merged_data: data.compute] {self.merged_data.get("data", {}).get("compute")}')
 
         return self.merged_data
 
@@ -194,10 +196,10 @@ class CollectionDataManager(BaseManager):
 
                 if self.update_mode == 'MERGE':
                     new_value = self._merge_old_and_new_value(old_value, new_value)
-                _LOGGER.debug(' ')
-                _LOGGER.debug(f'[_merge_data_from_history] {key}: {old_value} -> {new_value}')
-                _LOGGER.debug(f'[_merge_data_from_history] check priority: {new_priority <= old_priority and new_value != old_value}')
-                _LOGGER.debug(' ')
+                # _LOGGER.debug(' ')
+                # _LOGGER.debug(f'[_merge_data_from_history] {key}: {old_value} -> {new_value}')
+                # _LOGGER.debug(f'[_merge_data_from_history] check priority: {new_priority <= old_priority and new_value != old_value}')
+                # _LOGGER.debug(' ')
 
                 if new_priority <= old_priority and new_value != old_value:
                     history_info['diff'] = self._get_history_diff(old_value, new_value)
@@ -223,7 +225,7 @@ class CollectionDataManager(BaseManager):
     @staticmethod
     def _merge_old_and_new_value(old_value, new_value):
         if isinstance(new_value, dict) and isinstance(old_value, dict):
-            return utils.deep_merge(old_value, new_value)
+            return utils.deep_merge(new_value, old_value)
             # new_temp_value = copy.deepcopy(old_value)
             # new_temp_value.update(new_value)
             # return new_temp_value
