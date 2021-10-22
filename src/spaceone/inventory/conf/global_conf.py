b091ba2a33f39f3a8641dfcbee3e3b16f55e73c6
Jihyung Song <bluese@megazone.com>
2021-10-22 16:31:00 +0900

    add space connector in global_conf.py

--- src/spaceone/inventory/conf/global_conf.py
+++ src/spaceone/inventory/conf/global_conf.py
@@ -36,6 +36,15 @@ CONNECTORS = {
     },
     'ConfigConnector': {
     },
+    'SpaceConnector': {
+        'backend': 'spaceone.core.connector.space_connector.SpaceConnector',
+        'endpoints': {
+            'identity': 'grpc://identity:50051',
+            'plugin': 'grpc://plugin:50051',
+            'repository': 'grpc://repository:50051',
+            'secret': 'grpc://secret:50051',
+        }
+    },
 }
 
 ENDPOINTS = {}
