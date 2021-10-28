7a618caf84e82146268255773eb54cc6e4548b80
Jihyung Song <bluese@megazone.com>
2021-10-28 16:16:24 +0900

    convert spaceConnector

--- src/spaceone/inventory/manager/config_manager.py
+++ src/spaceone/inventory/manager/config_manager.py
@@ -10,10 +10,13 @@ class ConfigManager(BaseManager):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.config_conn: ConfigConnector = self.locator.get_connector('ConfigConnector')
+        # self.config_conn: ConfigConnector = self.locator.get_connector('ConfigConnector')
+        self.config_conn: SpaceConnector = self.locator.get_connector('SpaceConnector', service='config')
 
     def get_domain_config(self, name, domain_id):
-        result = self.config_conn.get_domain_config(name, domain_id)
+        # result = self.config_conn.get_domain_config(name, domain_id)
+        result = self.config_conn.dispatch('DomainConfig.get', {'name': name, 'domain_id': domain_id})
+
         rules = result['data']['rules']
         policies = {}
         for rule in rules:
