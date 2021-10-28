b3c34731cf05db4dbaa4e67fbb6628ece7f0ffd3
Jihyung Song <bluese@megazone.com>
2021-10-28 16:16:12 +0900

    convert spaceConnector

--- src/spaceone/inventory/manager/identity_manager.py
+++ src/spaceone/inventory/manager/identity_manager.py
@@ -6,16 +6,21 @@ class IdentityManager(BaseManager):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.identity_conn: IdentityConnector = self.locator.get_connector('IdentityConnector')
+        # self.identity_conn: IdentityConnector = self.locator.get_connector('IdentityConnector')
+        self.identity_conn: SpaceConnector = self.locator.get_connector('SpaceConnector', service='identity')
 
     def get_user(self, user_id, domain_id):
-        return self.identity_conn.get_user(user_id, domain_id)
+        return self.identity_conn.dispatch('User.get', {'user_id': user_id, 'domain_id': domain_id})
+        # return self.identity_conn.get_user(user_id, domain_id)
 
     def list_users(self, query, domain_id):
-        return self.identity_conn.list_users(query, domain_id)
+        return self.identity_conn.dispatch('User.list', {'query': query, 'domain_id': domain_id})
+        # return self.identity_conn.list_users(query, domain_id)
 
     def get_project(self, project_id, domain_id):
-        return self.identity_conn.get_project(project_id, domain_id)
+        return self.identity_conn.dispatch('Project.get', {'project_id': project_id, 'domain_id': domain_id})
+        # return self.identity_conn.get_project(project_id, domain_id)
 
     def list_projects(self, query, domain_id):
-        return self.identity_conn.list_projects(query, domain_id)
+        # return self.identity_conn.list_projects(query, domain_id)
+        return self.identity_conn.dispatch('Project.list', {'query': query, 'domain_id': domain_id})
