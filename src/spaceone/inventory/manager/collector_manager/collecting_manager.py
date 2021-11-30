8d4a1c0e6256e69d313b31552bf6785b8ad5da10
Jongmin Kim <whdalsrnt@megazone.com>
2021-11-30 18:44:19 +0900

    fix: fix collecting bug
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/inventory/manager/collector_manager/collecting_manager.py
+++ src/spaceone/inventory/manager/collector_manager/collecting_manager.py
@@ -239,14 +239,9 @@ class CollectingManager(BaseManager):
             else:
                 _LOGGER.debug(f'[collecting_resources] skip garbage_collection, {cleanup_mode}, {JOB_TASK_STATE}')
 
-            _LOGGER.debug("=====")
-            _LOGGER.debug(f"self.use_db_queue: {self.use_db_queue}")
-            _LOGGER.debug(f"ERROR: {ERROR}")
-            _LOGGER.debug(f"stat: {stat}")
-            _LOGGER.debug("=====")
             if self.use_db_queue and ERROR is False:
                 # WatchDog will finalize the task
-                # if ERROR occurred, there is no data to proncessing
+                # if ERROR occurred, there is no data to processing
                 pass
             else:
                 if self.use_db_queue:
@@ -255,7 +250,6 @@ class CollectingManager(BaseManager):
                 # Update Statistics of JobTask
                 self._update_job_task(job_task_id, JOB_TASK_STATE, domain_id, stat=stat)
                 # Update Job
-                _LOGGER.debug(f"== decrease_remained_tasks == {kwargs['job_id']}")
                 self.job_mgr.decrease_remained_tasks(kwargs['job_id'], domain_id)
 
         print("[collecting_resources] system tracker")
@@ -538,8 +532,9 @@ class CollectingManager(BaseManager):
             _LOGGER.debug(f'[_process_single_result] service error: {svc}, {e}')
             response = ERROR
         finally:
-            if resource_type == 'inventory.CloudServiceType':
-                response = NOT_COUNT
+            if response not in [CREATED, UPDATED]:
+                if resource_type in ['inventory.CloudServiceType', 'inventory.Region']:
+                    response = NOT_COUNT
             return response
 
     def _get_resource_map(self, resource_type):
