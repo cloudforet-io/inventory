2b5281b5d0bb91401c1d23b20e0bd1d88d532934
Jongmin Kim <whdalsrnt@megazone.com>
2022-12-07 20:06:24 +0900

    refactor: remove server APIs

--- src/spaceone/inventory/conf/proto_conf.py
+++ src/spaceone/inventory/conf/proto_conf.py
@@ -1,6 +1,5 @@
 PROTO = {
     'spaceone.inventory.api.v1.region': ['Region'],
-    'spaceone.inventory.api.v1.server': ['Server'],
     'spaceone.inventory.api.v1.collector': ['Collector'],
     'spaceone.inventory.api.v1.job': ['Job'],
     'spaceone.inventory.api.v1.job_task': ['JobTask'],
