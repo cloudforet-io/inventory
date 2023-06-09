d059fb7ca95a411a3ec10fef4ce5ce9bf66506bc
jiyungSong <bluese@megazone.com>
2023-06-09 11:12:58 +0900

    etc: Add logger for debug

--- src/spaceone/inventory/manager/collecting_manager.py
+++ src/spaceone/inventory/manager/collecting_manager.py
@@ -87,8 +87,9 @@ class CollectingManager(BaseManager):
 
         try:
             self.secret = secret_mgr.get_secret(secret_id, domain_id)
+            _LOGGER.debug(f'[collecting_resources] SECRET = {self.secret}')
             secret_data = secret_mgr.get_secret_data(secret_id, domain_id)
-
+            _LOGGER.debug(f'[collecting_resources] SECRET_DATA = {secret_data}')
         except Exception as e:
             _LOGGER.error(f'[collecting_resources] fail to get secret_data: {secret_id}')
             self.job_task_mgr.add_error(job_task_id, domain_id, 'ERROR_COLLECTOR_SECRET', e,
