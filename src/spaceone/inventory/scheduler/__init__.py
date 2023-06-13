219a778a7ec2fc5e4afbd747862c1ec58bf58af8
jiyungSong <bluese@megazone.com>
2023-06-13 18:47:12 +0900

    refactor: move to scheduler in interface

--- src/spaceone/inventory/scheduler/__init__.py
+++
@@ -1,3 +0,0 @@
-from spaceone.inventory.scheduler.inventory_scheduler import InventoryHourlyScheduler
-from spaceone.inventory.scheduler.cleanup_scheduler import CleanupScheduler
-from spaceone.inventory.scheduler.interval_scheduler import InventoryIntervalScheduler
