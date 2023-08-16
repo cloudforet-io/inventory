e4678b4eb2650bf7fc3ad9020b9e9dcf5249b0db
jihyungSong <bluese@megazone.com>
2023-08-16 14:52:49 +0900

    chore: add debug

--- src/spaceone/inventory/service/collector_service.py
+++ src/spaceone/inventory/service/collector_service.py
@@ -297,6 +297,7 @@ class CollectorService(BaseService):
 
             try:
                 response = collector_plugin_mgr.get_tasks(endpoint, secret_data, plugin_info.get('options', {}))
+                _LOGGER.debug(f'[get_tasks] response: {response}')
 
                 for task_options in response.get('tasks', []):
                     _task.update(task_options)
