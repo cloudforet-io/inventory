79dc216a090d31d5b7e9f9f307ef41eeb1eb5a2b
Jongmin Kim <whdalsrnt@megazone.com>
2023-11-08 20:45:38 +0900

    refactor: refactor message creation

--- src/spaceone/inventory/info/cloud_service_query_set_info.py
+++ src/spaceone/inventory/info/cloud_service_query_set_info.py
@@ -1,6 +1,6 @@
 import functools
+from google.protobuf.json_format import ParseDict
 from spaceone.api.inventory.v1 import cloud_service_query_set_pb2
-from spaceone.core.pygrpc.message_type import *
 from spaceone.core import utils
 from spaceone.inventory.model.cloud_service_query_set_model import CloudServiceQuerySet
 
@@ -20,20 +20,21 @@ def CloudServiceQuerySetInfo(cloud_svc_query_set_vo: CloudServiceQuerySet, minim
 
     if not minimal:
         info.update({
-            'query_options': change_analyze_query(cloud_svc_query_set_vo.query_options),
+            'query_options': cloud_svc_query_set_vo.query_options,
             'additional_info_keys': cloud_svc_query_set_vo.additional_info_keys,
             'data_keys': cloud_svc_query_set_vo.data_keys,
-            'unit': change_struct_type(cloud_svc_query_set_vo.unit),
-            'tags': change_struct_type(cloud_svc_query_set_vo.tags),
+            'unit': cloud_svc_query_set_vo.unit,
+            'tags': cloud_svc_query_set_vo.tags,
             'domain_id': cloud_svc_query_set_vo.domain_id,
             'created_at': utils.datetime_to_iso8601(cloud_svc_query_set_vo.created_at),
             'updated_at': utils.datetime_to_iso8601(cloud_svc_query_set_vo.updated_at)
         })
 
-    return cloud_service_query_set_pb2.CloudServiceQuerySetInfo(**info)
+    return info
 
 
 def CloudServiceQuerySetsInfo(cloud_svc_query_set_vos, total_count, **kwargs):
-    return cloud_service_query_set_pb2.CloudServiceQuerySetsInfo(
-        results=list(map(functools.partial(CloudServiceQuerySetInfo, **kwargs), cloud_svc_query_set_vos)),
-        total_count=total_count)
+    return ParseDict({
+        'results': list(map(functools.partial(CloudServiceQuerySetInfo, **kwargs), cloud_svc_query_set_vos)),
+        'total_count': total_count
+    }, cloud_service_query_set_pb2.CloudServiceQuerySetsInfo())
