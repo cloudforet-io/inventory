928950d819e9978ee5ea6bc164361afa4f56725c
seolmin <seolmin@megazone.com>
2023-12-03 23:43:41 +0900

    fix: fix get_yaml_path method

--- src/spaceone/inventory/plugin/collector/lib/metadata.py
+++ src/spaceone/inventory/plugin/collector/lib/metadata.py
@@ -2,25 +2,28 @@ import os
 import inspect
 import logging
 from spaceone.core.error import *
+from spaceone.core import config
 from spaceone.core.utils import load_yaml_from_file
 from spaceone.inventory.plugin.collector.lib.metadata_generator import MetadataGenerator
 
-__all__ = ['convert_cloud_service_meta', 'convert_cloud_service_type_meta']
+__all__ = ["convert_cloud_service_meta", "convert_cloud_service_type_meta"]
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def convert_cloud_service_meta(provider, cloud_service_group, cloud_service_type) -> dict:
+def convert_cloud_service_meta(
+    provider, cloud_service_group, cloud_service_type
+) -> dict:
     return {
-        'view': {
-            'sub_data': {
-                'reference': {
-                    'resource_type': 'inventory.CloudServiceType',
-                    'options': {
-                        'provider': provider,
-                        'cloud_service_group': cloud_service_group,
-                        'cloud_service_type': cloud_service_type,
-                    }
+        "view": {
+            "sub_data": {
+                "reference": {
+                    "resource_type": "inventory.CloudServiceType",
+                    "options": {
+                        "provider": provider,
+                        "cloud_service_group": cloud_service_group,
+                        "cloud_service_type": cloud_service_type,
+                    },
                 }
             }
         }
@@ -34,13 +37,12 @@ def convert_cloud_service_type_meta(metadata_path: str) -> dict:
 
 
 def _get_yaml_path(metadata_path: str) -> str:
-    # Get the previous path of call function
-    manager_path = inspect.stack()[3][1]
+    root_path = __import__(config.get_package()).__path__[0]
 
     try:
-        # Need to change to a better way
-        app_path, project, metadata, yaml_file = manager_path.rsplit('/', 3)
-        yaml_path = os.path.join(app_path, metadata_path)
+        yaml_path = os.path.join(root_path, metadata_path)
     except Exception as e:
-        raise ERROR_INVALID_PARAMETER(key='metadata_path', reason=f'Invalid metadata_path: {metadata_path}')
+        raise ERROR_INVALID_PARAMETER(
+            key="metadata_path", reason=f"Invalid metadata_path: {metadata_path}"
+        )
     return yaml_path
