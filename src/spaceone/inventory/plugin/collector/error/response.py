691fecd6811684a51a16022a97df221f9783ed74
Jongmin Kim <whdalsrnt@megazone.com>
2024-06-08 17:01:50 +0900

    fix: fix typo
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/inventory/plugin/collector/error/response.py
+++ src/spaceone/inventory/plugin/collector/error/response.py
@@ -2,17 +2,19 @@ from spaceone.core.error import *
 
 
 class ERROR_INVALID_PARAMETER(ERROR_INVALID_ARGUMENT):
-    _message = "A suitable field is required for resource_type. (inventory.CloudServiceType" \
-               "\ = cloud_service_type, inventory.CloudService = cloud_service, inventory.Region = region)"
+    _message = (
+        "A suitable field is required for resource_type. (inventory.CloudServiceType"
+        "\ = cloud_service_type, inventory.CloudService = cloud_service, inventory.Region = region)"
+    )
 
 
 class ERROR_NO_INPUT_FIELD(ERROR_INVALID_ARGUMENT):
     _message = 'Requires one of the fields "cloud_service_type", "cloud_service", and "region".'
 
 
-class ERROR_INVAILD_INPUT_FIELD(ERROR_INVALID_ARGUMENT):
+class ERROR_INVALID_INPUT_FIELD(ERROR_INVALID_ARGUMENT):
     _message = 'Must contain one of "cloud_service_type", "cloud_service", or "region". (current fields={fields})'
 
 
 class ERROR_NOT_MATCH_RESOURCE_TYPE(ERROR_INVALID_ARGUMENT):
-    _message = 'resource_type and resource do not match. (resource_type={resource_type}, resource={resource})'
+    _message = "resource_type and resource do not match. (resource_type={resource_type}, resource={resource})"
