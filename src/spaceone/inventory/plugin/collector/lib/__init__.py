0d9d6e4d3ae675500dad9fc35e9c55c8178c5cc9
Youngjin Jo <youngjinjo@megazone.com>
2024-07-25 23:48:01 +0900

    feat: add dependency for cloudforet inventory chart version 1.12.2
    Signed-off-by: Youngjin Jo <youngjinjo@megazone.com>

--- src/spaceone/inventory/plugin/collector/lib/__init__.py
+++ src/spaceone/inventory/plugin/collector/lib/__init__.py
@@ -1,3 +1,9 @@
 from spaceone.inventory.plugin.collector.lib.utils import *
 
-__all__ = ['make_cloud_service_type', 'make_cloud_service', 'make_response', 'make_error_response']
+__all__ = [
+    "make_cloud_service_type",
+    "make_cloud_service_with_metadata",
+    "make_cloud_service",
+    "make_response",
+    "make_error_response",
+]
